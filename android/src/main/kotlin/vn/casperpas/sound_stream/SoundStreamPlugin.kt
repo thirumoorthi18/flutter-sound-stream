740ec8907009d8975031d3d228f5ca313ff21208
thirumoorthi18 <thirumoorthim@coresolutionsinc.com>
2025-06-04 02:39:09 -0400

    NewUpdate SoundStreamPlugin.kt

--- android/src/main/kotlin/vn/casperpas/sound_stream/SoundStreamPlugin.kt
+++ android/src/main/kotlin/vn/casperpas/sound_stream/SoundStreamPlugin.kt
@@ -1,17 +1,29 @@
-class SoundStreamPlugin : FlutterPlugin, MethodCallHandler {
-    private lateinit var channel: MethodChannel
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.EventChannel
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_stream")
-        channel.setMethodCallHandler(this)
-        // initialize your plugin
-    }
+class SoundStreamPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
+  private lateinit var eventChannel: EventChannel
 
-    override fun onMethodCall(call: MethodCall, result: Result) {
-        // handle calls
-    }
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_stream")
+    channel.setMethodCallHandler(this)
+    
+    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "sound_stream_event")
+    // Set up your event channel handler here
+  }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-        channel.setMethodCallHandler(null)
-    }
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    // Handle method calls here
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
 }
+
