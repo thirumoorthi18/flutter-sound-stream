2cfa27d4934cace373f4e7375f54f17b9b655daa
thirumoorthi18 <thirumoorthim@coresolutionsinc.com>
2025-06-04 04:25:59 -0400

    latest SoundStreamPlugin.kt

--- android/src/main/kotlin/vn/casperpas/sound_stream/SoundStreamPlugin.kt
+++ android/src/main/kotlin/vn/casperpas/sound_stream/SoundStreamPlugin.kt
@@ -1,29 +1,24 @@
+package vn.casperpas.sound_stream
+
 import androidx.annotation.NonNull
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
-import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.EventChannel
 
-class SoundStreamPlugin: FlutterPlugin, MethodCallHandler {
-  private lateinit var channel : MethodChannel
-  private lateinit var eventChannel: EventChannel
+class SoundStreamPlugin : FlutterPlugin, MethodChannel.MethodCallHandler {
+    private lateinit var channel: MethodChannel
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_stream")
-    channel.setMethodCallHandler(this)
-    
-    eventChannel = EventChannel(flutterPluginBinding.binaryMessenger, "sound_stream_event")
-    // Set up your event channel handler here
-  }
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "sound_stream")
+        channel.setMethodCallHandler(this)
+    }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    // Handle method calls here
-  }
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(call: MethodCall, result: MethodChannel.Result) {
+        // TODO: Handle method calls
+        result.notImplemented()
+    }
 }
-
